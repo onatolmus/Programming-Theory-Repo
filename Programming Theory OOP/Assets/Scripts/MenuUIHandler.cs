b1b36c57292cfb653a6bc87e4ae1def65c2bd1d0
onatolmus <onatolmus@gmail.com>
2023-08-23 15:33:39 +0200

    third commit

--- Programming Theory OOP/Assets/Scripts/MenuUIHandler.cs
+++ Programming Theory OOP/Assets/Scripts/MenuUIHandler.cs
@@ -11,11 +11,6 @@ public class MenuUIHandler : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartGame()
     {
